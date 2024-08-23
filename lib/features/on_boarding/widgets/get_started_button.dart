4e6355dde1d9f545eddf7f1f719723ffb723d0cd
Taha Hamada <93037873+taha2901@users.noreply.github.com>
2024-08-23 16:30:15 +0300

    no message

--- lib/features/on_boarding/widgets/get_started_button.dart
+++ lib/features/on_boarding/widgets/get_started_button.dart
@@ -3,31 +3,33 @@ import 'package:doc/core/routings/routers.dart';
 import 'package:doc/core/theming/colors.dart';
 import 'package:doc/core/theming/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class GetStartedButton extends StatelessWidget {
   const GetStartedButton({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return TextButton(
-        style: ButtonStyle(
-          backgroundColor: WidgetStateProperty.all(ColorsManager.mainBlue),
-          tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-          maximumSize: WidgetStateProperty.all(
-            const Size(double.infinity, 52),
-          ),
-          shape:  WidgetStateProperty.all(
-            RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16),
-            )
-          )
-        ),
-        onPressed: () {
-          context.pushNamed(Routers.login);
-        },
-        child: Text(
-          'Get Started',
-          style: TextStyles.font16WhiteSemiBold,
-        ));
+    return SizedBox(
+      width: double.infinity,
+      height: 50.h,
+      child: TextButton(
+          style: ButtonStyle(
+              backgroundColor: WidgetStateProperty.all(ColorsManager.mainBlue),
+              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              // maximumSize: WidgetStateProperty.all(
+              //   const Size(double.infinity, 52),
+              // ),
+              shape: WidgetStateProperty.all(RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ))),
+          onPressed: () {
+            context.pushNamed(Routers.login);
+          },
+          child: Text(
+            'Get Started',
+            style: TextStyles.font16WhiteSemiBold,
+          )),
+    );
   }
 }
