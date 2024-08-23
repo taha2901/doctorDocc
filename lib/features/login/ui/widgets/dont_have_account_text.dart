a83b274e0caf219cbc1a546e16d5e03e927b1b86
Taha Hamada <93037873+taha2901@users.noreply.github.com>
2024-08-23 20:49:09 +0300

    - added apacing file for Sized box. - added some colors & textStyles. - added fontWeightHelperClass. - added Custom  TextformField. - addded Custom Button. - added dont have account widget. - added terms and conditions text widget. - added login screen

---
+++ lib/features/login/ui/widgets/dont_have_account_text.dart
@@ -0,0 +1,33 @@
+import 'package:doc/core/helpers/extentions.dart';
+import 'package:doc/core/routings/routers.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../core/theming/styles.dart';
+
+class DontHaveAccountText extends StatelessWidget {
+  const DontHaveAccountText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      textAlign: TextAlign.center,
+      text: TextSpan(
+        children: [
+          TextSpan(
+            text: 'Don\'t have an account?',
+            style: TextStyles.font13DarkBlueRegular,
+          ),
+          TextSpan(
+            text: ' Sign Up',
+            style: TextStyles.font13BlueSemiBold,
+            recognizer: TapGestureRecognizer()
+              ..onTap = () {
+                // context.pushReplacementNamed(Routers.signUp);
+              },
+          ),
+        ],
+      ),
+    );
+  }
+}
