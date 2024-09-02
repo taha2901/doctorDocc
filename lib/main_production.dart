59fc598bb0132bc59b4b265a554aa370643f5f4d
Taha Hamada <93037873+taha2901@users.noreply.github.com>
2024-09-02 18:50:40 +0300

    - added shared pref package. - added secure storage package. - Save User Token and Secured it is Done.✅

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,14 +1,28 @@
 import 'package:doc/core/di/dependency_injection.dart';
+import 'package:doc/core/helpers/constants.dart';
+import 'package:doc/core/helpers/extentions.dart';
+import 'package:doc/core/helpers/shared_pref_helper.dart';
 import 'package:doc/core/routings/app_router.dart';
 import 'package:doc/doc_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main()async {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   setUpGetIt();
   // To fix texts being hidden  bug in flutter_screenutil in release mode
   await ScreenUtil.ensureScreenSize();
+  await checkLoggedInUser();
   runApp(DocApp(
     appRouter: AppRouter(),
   ));
+}
+
+checkLoggedInUser() async {
+  String? userToken = await SharedPrefHelper.getSecuredString(SharedPrefKeys.userToken);
+  if (userToken.isNullOrEmpty()) {
+    isLoggedInUser = true;
+  } else {
+    isLoggedInUser = false;
+  }
 }
